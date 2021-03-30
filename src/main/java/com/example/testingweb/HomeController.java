d7d6b90c1bfa70c23e07ca47f0847e27530cf8a8
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-03-31 01:09:38 +0900

    spotbugs, pmd test

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -6,10 +6,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
+		String unused1 = "2";
+		String unused = null;
+		if(unused.equals("unused")){
+			return "world";
+		}
 		return "Hello, World";
 	}
 
+	@RequestMapping("/s")
+	public @ResponseBody String greeting2() {
+		return null;
+	}
 }
